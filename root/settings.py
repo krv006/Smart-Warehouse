67d8ba5db3e192bdef89917c6009f7fafbf5b4d1
krv006 <rvkamronbek@gmail.com>
2026-06-24 00:37:52 +0500

    settings

--- root/settings.py
+++ root/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
